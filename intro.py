21d5868f31e7d6cbd2c12e688bd60d31495af219
Roberto de Cecilio <robertodececilio@gmail.com>
2015-11-01 14:32:38 +0000

    1.00c
    Minor fixes

--- intro.py
+++ intro.py
@@ -18,7 +18,7 @@ def intro(globalApp):
     
     introtext = tk.Label(introCanvas, text="In this experiment, you will be " + 
     "performing a mechanical task known as the UNRAVEL task.\n Its " + 
-    "instructions will be explained by both the supervisor and the next" + 
+    "instructions will be explained by both the supervisor and the next " + 
     "screen \n\n\n\n You will also have to memorize a series of pictures and" +
     "then recognize them\n from a series of other pictures which  will be " +
     "rotated in 0, 90 and 180 degrees.\n\n\n\n Press any key to continue.",
